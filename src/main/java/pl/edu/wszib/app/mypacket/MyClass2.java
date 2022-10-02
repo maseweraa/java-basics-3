2fa7fdfb2a2ee4dc524c369432e18ab2722177eb
Marcel Seweryn <masewera@student.wszib.edu.pl>
2022-10-02 15:15:08 +0200

    Initial commit

---
+++ src/main/java/pl/edu/wszib/app/mypacket/MyClass2.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.app.mypacket;
+
+import pl.edu.wszib.app.MyClass;
+public class MyClass2 extends MyClass {
+
+    //public MyClass2 myClass;
+
+    public void run() {
+        field3 = "Field3 from extended";
+        run3();
+     //   myClass.run4();
+    }
+}
